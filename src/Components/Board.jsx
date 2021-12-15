d8269456619fbd4832362423eee98fa5938296be
glowing713 <glowing713@naver.com>
2021-12-15 18:24:28 +0900

    refactor: Change Board component controlled by Game

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -2,80 +2,19 @@ import React from "react";
 import Square from "./Square";
 
 export default class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      board: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
-
-  getNextPlayer() {
-    return this.state.xIsNext ? "X" : "O";
-  }
-
-  handleClick(i) {
-    const board = this.state.board.slice();
-    if (this.calculateWinner() || board[i]) {
-      return;
-    }
-    board[i] = this.getNextPlayer();
-    this.setState({ board, xIsNext: !this.state.xIsNext });
-  }
-
   renderSquare(i) {
     // Square 컴포넌트에 클릭 이벤트가 발생하면 onClick prop인 handleClick 함수를 전달한다.
     return (
       <Square
-        value={this.state.board[i]}
-        onClick={() => {
-          // Square 컴포넌트 내부의 button이 클릭되면 prop으로 넘겨받은 onClick 함수(handleClick)가 실행된다.
-          // 이때, handleClick 내부의 this를 콘솔에 찍어보면 Board 클래스에 바인딩되어있는 것을 확인할 수 있다.
-          this.handleClick(i);
-        }}
+        value={this.props.board[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
 
-  calculateWinner() {
-    const { board, winner } = this.state;
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; ++i) {
-      const [a, b, c] = lines[i];
-      if (
-        !winner &&
-        board[a] &&
-        board[a] === board[b] &&
-        board[a] === board[c]
-      ) {
-        return board[a];
-      }
-    }
-    return null;
-  }
-
   render() {
-    const winner = this.calculateWinner(); // 승부 계산
-
-    let gameStatus;
-    if (winner) {
-      gameStatus = `Winner: ${winner}`;
-    } else {
-      gameStatus = `Next player: ${this.getNextPlayer()}`;
-    }
-
     return (
       <div>
-        <div className="status">{gameStatus}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -2,14 +2,78 @@ import React from "react";
 import Board from "./Board";
 
 export default class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          board: Array(9).fill(null),
+        },
+      ],
+      xIsNext: true,
+    };
+  }
+
+  getNextPlayer() {
+    return this.state.xIsNext ? "X" : "O";
+  }
+
+  calculateWinner(board) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; ++i) {
+      const [a, b, c] = lines[i];
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        return board[a];
+      }
+    }
+    return null;
+  }
+
+  handleClick(i) {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const board = current.board.slice();
+    if (this.calculateWinner(board) || board[i]) {
+      return;
+    }
+    board[i] = this.getNextPlayer();
+    this.setState({
+      history: history.concat([
+        {
+          board,
+        },
+      ]),
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = this.calculateWinner(current.board);
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board board={current.board} onClick={(i) => this.handleClick(i)} />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
