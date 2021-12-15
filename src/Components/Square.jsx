1a8a9b9a5473fa0dcef58d78b307d2c70e300f59
glowing713 <glowing713@naver.com>
2021-12-15 15:21:24 +0900

    feat: Add clicked state to Square component

--- src/Components/Square.jsx
+++ src/Components/Square.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 
 export default class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            clicked: true
+        };
+    }
+
     render() {
+        let clickState = this.state.clicked;
       return (
-        <button className="square">
-          {this.props.value}
+        <button className="square" onClick={() => this.setState({clicked: !clickState})}>
+          {clickState ? this.props.value : 'X'}
         </button>
       );
     }
