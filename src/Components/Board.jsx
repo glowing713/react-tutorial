e5a9dffdf7b387c3538f2e713518624670acdb4b
glowing713 <glowing713@naver.com>
2021-12-15 18:05:45 +0900

    feat: Calculate winner

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -10,13 +10,16 @@ export default class Board extends React.Component {
     };
   }
 
-  nextPlayer() {
+  getNextPlayer() {
     return this.state.xIsNext ? "X" : "O";
   }
 
   handleClick(i) {
     const board = this.state.board.slice();
-    board[i] = this.nextPlayer();
+    if (this.calculateWinner() || board[i]) {
+      return;
+    }
+    board[i] = this.getNextPlayer();
     this.setState({ board, xIsNext: !this.state.xIsNext });
   }
 
@@ -34,12 +37,45 @@ export default class Board extends React.Component {
     );
   }
 
+  calculateWinner() {
+    const { board, winner } = this.state;
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; ++i) {
+      const [a, b, c] = lines[i];
+      if (
+        !winner &&
+        board[a] &&
+        board[a] === board[b] &&
+        board[a] === board[c]
+      ) {
+        return board[a];
+      }
+    }
+    return null;
+  }
+
   render() {
-    const status = `Next player: ${this.nextPlayer()}`;
+    const winner = this.calculateWinner(); // 승부 계산
+
+    let gameStatus;
+    if (winner) {
+      gameStatus = `Winner: ${winner}`;
+    } else {
+      gameStatus = `Next player: ${this.getNextPlayer()}`;
+    }
 
     return (
       <div>
-        <div className="status">{status}</div>
+        <div className="status">{gameStatus}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
